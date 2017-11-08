f8a38790d921685aba43b6f55ddd3c75e9dc3ee1
Sam Glerum <samuelglerum@hotmail.com>
2017-11-08 20:58:19 +0100

    .

--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request, question_id):
+def detail(request, question_id):
     return HttpResponse("You're looking at question {}.".format(question_id))
 
 def results(request, question_id):
